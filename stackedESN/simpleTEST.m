20863ea6013e6039c220c9525cc57337220c46e6
oOoDKoOo <dakrefl@macth84.cern.ch>
2016-07-29 17:05:45 +0200

    with MC runs

--- stackedESN/simpleTEST.m
+++ stackedESN/simpleTEST.m
@@ -3,6 +3,8 @@
 data = mackeyglass(1000);
 testp = 0.5;
 
+MCruns = 100;
+
 split_point = round((1-testp) * length(data)); % take testp as hold out test set
 
 train_input  = data(1:split_point, :);
@@ -11,16 +13,34 @@ test_input   = data(split_point + 1:end - 1, :);
 test_output  = data(split_point + 2:end, :);
 
 % ESN settings
-N = [5, 5];
-connectivity = [0.5 0.5];
+N = [10, 10];
+connectivity = [0.1 0.1];
 sp_radius = [0.1 1.0];
 lr = [0.1 0.9];
 sigma_noise = 0.01;
 
-[Win, W] = buildStackedESN(size(train_input,2), N, connectivity, sp_radius, 'randn');
-[Wout, states, states_evolution] = trainStackedESN(train_input, train_output, Win, W, lr, sigma_noise, 'tanh', 'linear');
-Y = runStackedESN(test_input(1,:), length(test_output), states, Win, W, Wout, lr, sigma_noise, 'tanh', 'linear', 1);
+tic
 
+% Monte Carlo runs
+M = [];
+for r = 1 : MCruns 
+
+
+    [Win, W] = buildStackedESN(size(train_input,2), N, connectivity, sp_radius, 'randn');
+    [Wout, states, states_evolution] = trainStackedESN(train_input, train_output, Win, W, lr, sigma_noise, 'tanh', 'linear');
+    Y = runStackedESN(test_input(1,:), length(test_output), states, Win, W, Wout, lr, sigma_noise, 'tanh', 'linear', 1);
+
+    error = RMSE(Y(:,1),test_output(:,1));
+    
+    if error < 1
+        M = [M Y];
+    end
+end
+
+% Average over results
+Y = mean(M,2);
+
+% Plot
 figure('name','ESN test'), 
 subplot(2,1,1), hold on
 axis([1,size(states_evolution,2), -1, 1])
@@ -38,4 +58,4 @@ subplot(2,1,2), hold on
 title('ESN test');
 plot(test_output(:,1));
 plot(Y(:,1));
-
+toc
