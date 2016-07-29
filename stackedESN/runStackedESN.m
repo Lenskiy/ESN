ae6828947df0cc79d660ca38ee7d086ad4777907
Lenskiy <a.a.lensky@gmail.com>
2016-07-29 23:45:02 +1000

    Modified the part of the code that creats additional states

--- stackedESN/runStackedESN.m
+++ stackedESN/runStackedESN.m
@@ -1,11 +1,12 @@
 
-function Y = runStackedESN(initial_vector, Nsamples,  x, Win, W, Wout, lr, sigma, node_type, output_type, feedback_scaling )
+function Y = runStackedESN(initial_input, Nsamples,  x, Win, W, Wout, lr, sigma, node_type, output_type, feedback_scaling )
     % in generative mode no need to initialize the states i.e. x, 
     % they have already been initialized
     % only one initial input vector is need to start generating the output
     outSize = size(Wout,1);
     Y = zeros(Nsamples, outSize);
-    u = initial_vector';
+    u = initial_input';
+    input_size = length(u);
     
     switch(node_type)
         case 'tanh'
@@ -38,14 +39,17 @@ function Y = runStackedESN(initial_vector, Nsamples,  x, Win, W, Wout, lr, sigma
             x_total = [x_total; x{k}];
         end
         
-        switch (output_type)
+        switch output_type
             case 'linear'
-                
+
             case 'quadratic'
-                X = nchoosek(x_total(:,1),2);
-                Y_temp = X(:,1).*X(:,2);
-        
-                x_total = [x_total; Y_temp];
+                % skip the bias and input vector
+                x_org = x_total(1 + input_size + 1:end, :);  
+            	X = nchoosek(x_org,2);
+            	X_ = X(:,1).*X(:,2);
+            	% include bais, inputs, ESN states, squared ESN states, and producs of
+            	% all combinations of states: states = {x,y}, then we have {1, u, x, y, x^2, y^2, xy}
+                x_total = [x_total; x_org.^2; X_];
         end
         
         y = Wout*x_total;
--- stackedESN/trainStackedESN.m
+++ stackedESN/trainStackedESN.m
@@ -1,5 +1,5 @@
 
-function [Wout, states, states_evolution_org] = trainStackedESN(input,...
+function [Wout, x, x_evolution_org] = trainStackedESN(input,...
                                                         target, Win, W, lr, sigma, node_type, output_type)
     initLen = 0;
     trainLen = size(input,1);
@@ -25,7 +25,7 @@ function [Wout, states, states_evolution_org] = trainStackedESN(input,...
         states_size = states_size + size(W{k},1);
     end    
 
-    states_evolution = zeros(1 + input_size + states_size,...
+    x_evolution = zeros(1 + input_size + states_size,...
                                                       trainLen - initLen);
     % run the reservoir with the data and collect states
 
@@ -33,7 +33,7 @@ function [Wout, states, states_evolution_org] = trainStackedESN(input,...
     for j = 1:trainLen
         u = input(j, :)';
         cur_state_ind = 1;
-        states_evolution(cur_state_ind:input_size + 1,j - initLen) = [1; u];
+        x_evolution(cur_state_ind:input_size + 1,j - initLen) = [1; u];
         cur_state_ind = cur_state_ind + input_size + 1;
         for k = 1:length(W)
             switch (node_type)
@@ -50,33 +50,35 @@ function [Wout, states, states_evolution_org] = trainStackedESN(input,...
             x{k} = (1 - lr(k)) * x{k} + lr(k) * x_updated;
             
             if j > initLen % don't store a first few input states 
-                states{k} = x{k};
-                states_evolution(cur_state_ind:cur_state_ind + size(x{k},1) - 1, j - initLen) = states{k};
+                x_evolution(cur_state_ind:cur_state_ind + size(x{k},1) - 1, j - initLen) = x{k};
                 cur_state_ind = cur_state_ind + length(x{k});
             end
         end
     end
     
-    % skip the bias and input vector
-    states_evolution_org = states_evolution(1 + input_size + 1:end, :);
-    
-    switch output_type
+
+  	switch output_type
         case 'linear'
             
         case 'quadratic'
-            % create additional nodes which are products reservior states
-            O = [];
-            for i = 1:length(states_evolution) 
-                X = nchoosek(states_evolution(:,i),2);
-                Y = X(:,1).*X(:,2);
-                O = [O Y];
+            % skip the bias and input vector
+            x_evolution_org = x_evolution(1 + input_size + 1:end, :);
+            %pre-locate memory
+            x_evolution_plus = zeros((states_size - 1) * states_size / 2 + 2 * states_size + input_size + 1, size(x_evolution_org,2));
+
+            for i = 1:size(x_evolution_org,2)
+                X = nchoosek(x_evolution_org(:,i),2);
+                X_ = X(:,1).*X(:,2);
+                % include bais, inputs, ESN states, squared ESN states, and producs of
+                % all combinations of states: states = {x,y}, then we have {1, u, x, y, x^2, y^2, xy}
+                x_evolution_plus(:, i) = [x_evolution(:, i); x_evolution_org(:,i).^2; X_];
             end
-            states_evolution = [states_evolution; O];
+            x_evolution = x_evolution_plus;
     end
     
     
     % Train ouput layer by solving a system of linear equations 
-    Xinv = pseudoinverse(states_evolution, [],'lsqr');
+    Xinv = pseudoinverse(x_evolution, [],'lsqr');
     Wout = target(initLen + 1:end, :)' * Xinv; 
     
     % Add here the Wiener-Hopf solution
