5a4b0cc070f1ead25ff52841d80431dfc5f7d133
Lenskiy <a.a.lensky@gmail.com>
2016-09-20 00:15:09 +0900

    better paramters for Lorenz demo

--- ver4graphESN/testLorenzgeneration.m
+++ ver4graphESN/testLorenzgeneration.m
@@ -20,16 +20,16 @@ net = Network();
 lID(1) = net.addLayer(1, 'bias',   struct('nodeType', 'linear', 'leakage', 1, 'initType', 'randn')); % All layers have bias
 lID(2) = net.addLayer(size(train_input,1), 'input',  struct('nodeType', 'linear', 'leakage', 1.0));
 lID(3) = net.addLayer(size(train_output,1), 'output', struct('nodeType', 'linear', 'leakage', 1.0));
-lID(4) = net.addLayer(100, 'reservoir', struct('nodeType', 'tanh', 'radius', 1.0, 'leakage', 0.2, 'connectivity',0.25, 'initType', 'randn'));
+lID(4) = net.addLayer(200, 'reservoir', struct('nodeType', 'tanh', 'radius', 1.0, 'leakage', 0.2, 'connectivity',0.25, 'initType', 'randn'));
 lID(5) = net.addLayer(100, 'reservoir', struct('nodeType', 'tanh', 'radius', 0.6, 'leakage', 0.6, 'connectivity',0.15, 'initType', 'randn'));
-lID(6) = net.addLayer(100, 'reservoir', struct('nodeType', 'tanh', 'radius', 0.5, 'leakage', 1.0, 'connectivity',0.1, 'initType', 'randn'));
+lID(6) = net.addLayer(50, 'reservoir', struct('nodeType', 'tanh', 'radius', 0.5, 'leakage', 1.0, 'connectivity',0.1, 'initType', 'randn'));
 
 connections = [1.0, 1.0, 1.0, 1.0  1.0 0.01 0.01 0.01];
 arch = sparse([lID(2) lID(2) lID(1) lID(1) lID(4) lID(4) lID(5) lID(6) ],...
               [lID(4) lID(3) lID(3) lID(4) lID(3) lID(5) lID(6) lID(4)], connections,7,7);
 net.setConnections(arch, 'randn');
 
-% net.visualize();
+ net.visualize();
 
 %% Train the network
 boltrain = BatchOutputLayerTrain();
